e809f11873b6516355c8756305b91e3d31c100ce
Parsa Ghasemzadeh <parsa.2286213@gmail.com>
2026-07-15 15:44:14 +0330

    Start CS50P Lecture 0

---
+++ Lecture0/hello.py
@@ -0,0 +1,5 @@
+# Ask user's name
+name = input("What's your name? ")
+
+# Say hello to the user
+print("hello, {name} ")
---
+++ Lecture0/notes.md
@@ -0,0 +1,8 @@
+Today-1:
+Python runs through terminal
+print()
+variables
+input()
+strings
+f strings
+named parameters
